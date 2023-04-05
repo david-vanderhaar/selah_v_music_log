8f4200d97dc2569fec48da08fff39ac3f2f0066b
David Vanderhaar <d.vanderhaarhunter@gmail.com>
2023-04-05 17:44:40 -0500

    add fonts, image animation;

--- src/aws.js
+++ src/aws.js
@@ -29,23 +29,45 @@ async function getAllObjects() {
 
 function addObjectsToPlaylist(objects) {
   const playlist = document.getElementById('playlist');
-  objects.forEach(function(object) {
+  objects.forEach(function(object, index) {
     const key = object.Key;
     const url = 'https://' + BUCKET + '.s3.amazonaws.com/' + key;
     const li = document.createElement('li');
     const title = key.split('/').at(-1)
     if (title) {
-      li.innerHTML = title.split('.')[0];
+      const text = title.split('.')[0];
+      const alien_text = shuffleText(text)
+      const id = 'track' + '-' + index
+      li.id = id
       li.addEventListener('click', function() {
         const audio = document.getElementById('music-player');
         audio.src = url;
         audio.play();
       });
       playlist.appendChild(li);
+      new Typed(`#${id}`, {
+        strings: [alien_text, text],
+        typeSpeed: 10,
+        startDelay: Math.random() * 500,
+        smartBackspace: true,
+        backSpeed: 20,
+        showCursor: false,
+      });
     }
   });
 }
 
 function handleError(error) {
   console.log(error, error.stack);
+}
+
+function shuffleArray(unshuffled) {
+  return unshuffled
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+}
+
+function shuffleText(unshuffled) {
+  return shuffleArray(unshuffled.split('')).join('')
 }
